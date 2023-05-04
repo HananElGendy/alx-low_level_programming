68a0e7d0e2986f621ce23ae7b29f23f330b1e2b3
Hanan ElGendy <hanan.a.elgendy@gmail.com>
2023-05-04 15:20:48 -0700

    101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,44 +1,35 @@
 #include "main.h"
+
 /**
- * print_number - Converts Integer to string
- * @n: Input integer
- * Return: void
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
 void print_number(int n)
 {
-	int i;
-	int len;
-	int j;
-	int mux;
-	int out;
+	unsigned int m, d, count;
 
-	len = 0;
 	if (n < 0)
 	{
-		_putchar('-');
-		n *= -1;
-	}
-	i = n;
-	if (n == 0)
-	{
-		_putchar('0');
+		_putchar(45);
+		m = n * -1;
 	}
 	else
 	{
-		while (i / 10 != 0)
-		{
-			len++;
-			i /= 10;
-		}
-		mux = 1;
-		for (j = 1; j <= len; j++)
-			mux *= 10;
-		for (j = 0; j <= len; j++)
-		{
-			out = n / mux;
-			_putchar(out + '0');
-			n -= (mux * out);
-			mux /= 10;
-		}
+		m = n;
+	}
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
 	}
 }
